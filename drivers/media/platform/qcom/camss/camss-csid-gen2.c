e2cfd16756e0ee3612599ece6d66697024676154
Joel Selvaraj <jo@jsfamily.in>
2022-02-01 07:52:50 +0530

    camss: some logs

--- drivers/media/platform/qcom/camss/camss-csid-gen2.c
+++ drivers/media/platform/qcom/camss/camss-csid-gen2.c
@@ -484,7 +484,7 @@ static u32 csid_hw_version(struct csid_device *csid)
 	hw_gen = (hw_version >> HW_VERSION_GENERATION) & 0xF;
 	hw_rev = (hw_version >> HW_VERSION_REVISION) & 0xFFF;
 	hw_step = (hw_version >> HW_VERSION_STEPPING) & 0xFFFF;
-	dev_dbg(csid->camss->dev, "CSID HW Version = %u.%u.%u\n",
+	dev_info(csid->camss->dev, "CSID HW Version = %u.%u.%u\n",
 		hw_gen, hw_rev, hw_step);
 
 	return hw_version;
