1ba86a17e769ff6ba640c949ffca113cbf584ce4
Alex Elder <elder@linaro.org>
2022-12-30 17:22:27 -0600

    net: ipa: enable IPA interrupt handlers separate from registration
    Expose ipa_interrupt_enable() and have functions that register
    IPA interrupt handlers enable them directly, rather than having the
    registration process do that.  Do the same for disabling IPA
    interrupt handlers.
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/ipa/ipa_interrupt.h
+++ drivers/net/ipa/ipa_interrupt.h
@@ -86,6 +86,20 @@ void ipa_interrupt_suspend_clear_all(struct ipa_interrupt *interrupt);
 void ipa_interrupt_simulate_suspend(struct ipa_interrupt *interrupt);
 
 /**
+ * ipa_interrupt_enable() - Enable an IPA interrupt type
+ * @ipa:	IPA pointer
+ * @ipa_irq:	IPA interrupt ID
+ */
+void ipa_interrupt_enable(struct ipa *ipa, enum ipa_irq_id ipa_irq);
+
+/**
+ * ipa_interrupt_disable() - Disable an IPA interrupt type
+ * @ipa:	IPA pointer
+ * @ipa_irq:	IPA interrupt ID
+ */
+void ipa_interrupt_disable(struct ipa *ipa, enum ipa_irq_id ipa_irq);
+
+/**
  * ipa_interrupt_config() - Configure the IPA interrupt framework
  * @ipa:	IPA pointer
  *
